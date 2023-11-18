ea22460abf683bcdf9b9f9b656472edf2b2d8577
Blake Jones <blakeejoness@gmail.com>
2023-11-18 07:53:55 +0000

    Started setting up MUI theming

--- wordSeeker/src/App.tsx
+++ wordSeeker/src/App.tsx
@@ -1,11 +1,33 @@
-import { Typography } from '@mui/material'
+import { FormControlLabel, FormGroup, Switch, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 
-function App() {
+export default function App() {
+  type Mode = 'light' | 'dark'
+  const [mode, setMode] = useState<Mode>(() => {
+    return (localStorage.getItem('theme') as Mode) ?? 'light'
+  })
+  const [checked, setChecked] = useState<boolean>(true)
+  useEffect(() => {
+    if (mode == 'light') {
+      setChecked(true)
+    } else {
+      setChecked(false)
+    }
+    localStorage.setItem('theme', mode)
+  }, [mode])
   return (
     <>
       <Typography variant="h1">wordSeeker</Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={checked} />}
+          label={`Currently using ${mode} mode`}
+          onChange={(_, checked) => {
+            if (checked) setMode('light')
+            else setMode('dark')
+          }}
+        />
+      </FormGroup>
     </>
   )
 }
-
-export default App
