8b83363542d1e3fdd5a718c43a85f70cf820b6eb
Blake Jones <blakeejoness@gmail.com>
2023-11-20 03:16:28 +0000

    Update base URL in vite.config.ts

--- wordSeeker/vite.config.ts
+++ wordSeeker/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: 'https://blakee-joness.github.io/wordseeker/',
+  base: '/wordseeker/',
   preview: {
     host: true,
     port: 8080,
