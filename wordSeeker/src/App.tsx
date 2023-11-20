528b5555740ef9ed1fe25f08bb2605048a8f0684
Blake Jones <blakeejoness@gmail.com>
2023-11-20 01:58:29 +0000

    Add PaletteTest component

--- wordSeeker/src/App.tsx
+++ wordSeeker/src/App.tsx
@@ -1,23 +1,15 @@
 import Footer from '@Components/Footer'
 import Header from '@Components/Header'
 import ContainerGrid from '@Pages/ContainerGrid'
+import PaletteTest from '@Pages/PaletteTest'
 import ContextProvider from '@Providers/ContextProvider'
-import { Button } from '@mui/material'
 
 export default function App(): JSX.Element {
   return (
     <ContextProvider>
       <Header />
       <ContainerGrid>
-        <Button color="primary" variant="contained">
-          Primary
-        </Button>
-        <Button color="secondary" variant="contained">
-          Secondary
-        </Button>
-        <Button color="error" variant="contained">
-          Error
-        </Button>
+        <PaletteTest />
       </ContainerGrid>
       <Footer />
     </ContextProvider>
