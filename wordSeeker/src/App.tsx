d4b3191343e11b315a19f28cedc1aa55e4e74348
Blake Jones <blakeejoness@gmail.com>
2023-11-20 00:25:44 +0000

    Refactor button layout and add header and footer components

--- wordSeeker/src/App.tsx
+++ wordSeeker/src/App.tsx
@@ -1,12 +1,25 @@
+import Footer from '@Components/Footer'
+import Header from '@Components/Header'
+import ContainerGrid from '@Pages/ContainerGrid'
 import ContextProvider from '@Providers/ContextProvider'
 import { Button } from '@mui/material'
 
 export default function App(): JSX.Element {
   return (
     <ContextProvider>
-      <Button color="primary">Primary</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="error">Error</Button>
+      <Header />
+      <ContainerGrid>
+        <Button color="primary" variant="contained">
+          Primary
+        </Button>
+        <Button color="secondary" variant="contained">
+          Secondary
+        </Button>
+        <Button color="error" variant="contained">
+          Error
+        </Button>
+      </ContainerGrid>
+      <Footer />
     </ContextProvider>
   )
 }
