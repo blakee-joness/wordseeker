76eee1e6424b3ac17b0dd36a7b23d9fbf69e3225
Blake Jones <blakeejoness@gmail.com>
2023-11-20 03:17:11 +0000

    Add GitHub link to Footer component

--- wordSeeker/src/Components/Footer.tsx
+++ wordSeeker/src/Components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Link, Typography, useTheme } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 export default function Footer(): JSX.Element {
   const theme = useTheme()
@@ -7,15 +8,23 @@ export default function Footer(): JSX.Element {
     <Box sx={{ bgcolor: 'primary.light', p: 2, position: 'fixed', bottom: 0, width: '100%' }}>
       <Container maxWidth="sm" sx={{ color: theme.palette.common.white }}>
         <Typography variant="body2" align="center">
-          Created by{' '}
+          {'Created by '}
           <Link
             target="none"
             href="https://github.com/blakee-joness/"
             sx={{ color: theme.palette.common.white, fontWeight: 'bold' }}
           >
             @blakee-joness
-          </Link>{' '}
-          {new Date().getFullYear()}
+          </Link>
+          {' • Source on '}
+          <Link
+            target="none"
+            href="https://github.com/blakee-joness/wordseeker/"
+            sx={{ color: theme.palette.common.white, fontWeight: 'bold' }}
+          >
+            GitHub <GitHubIcon fontSize="small" />
+          </Link>
+          {` • ${new Date().getFullYear()}`}
         </Typography>
       </Container>
     </Box>
