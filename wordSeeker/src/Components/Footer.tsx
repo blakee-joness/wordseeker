3eb5405b3e822728fb1c298c774b312fe79a94e9
Blake Jones <blakeejoness@gmail.com>
2023-11-20 00:24:56 +0000

    Add Footer component

---
+++ wordSeeker/src/Components/Footer.tsx
@@ -0,0 +1,23 @@
+import { Box, Container, Link, Typography, useTheme } from '@mui/material'
+
+export default function Footer(): JSX.Element {
+  const theme = useTheme()
+
+  return (
+    <Box sx={{ bgcolor: 'primary.light', p: 2, position: 'fixed', bottom: 0, width: '100%' }}>
+      <Container maxWidth="sm" sx={{ color: theme.palette.common.white }}>
+        <Typography variant="body2" align="center">
+          Created by{' '}
+          <Link
+            target="none"
+            href="https://github.com/blakee-joness"
+            sx={{ color: theme.palette.common.white, fontWeight: 'bold' }}
+          >
+            @blakee-joness
+          </Link>{' '}
+          {new Date().getFullYear()}
+        </Typography>
+      </Container>
+    </Box>
+  )
+}
