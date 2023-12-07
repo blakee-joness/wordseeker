1cfe69e7d2cfcec7b8e2940cd1d4ca10e9c02ad0
Blake Jones <blakeejoness@gmail.com>
2023-12-07 04:15:51 +0000

    Update grid layout in ContainerGrid component

--- wordSeeker/src/Pages/ContainerGrid.tsx
+++ wordSeeker/src/Pages/ContainerGrid.tsx
@@ -3,12 +3,12 @@ import { Box, Grid } from '@mui/material'
 export default function ContainerGrid({ children }: { children?: JSX.Element | JSX.Element[] }): JSX.Element {
   return (
     <Box sx={{ flexGrow: 1, m: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}></Grid>
+      <Grid container>
+        <Grid item xs={0} md={4}></Grid>
         <Grid item xs={12} md={4}>
           {children}
         </Grid>
-        <Grid item xs={12} md={4}></Grid>
+        <Grid item sm={0} md={4}></Grid>
       </Grid>
     </Box>
   )
