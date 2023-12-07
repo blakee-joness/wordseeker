688b1d044fd11e9e6e8d37b5f373101481acb983
Blake Jones <blakeejoness@gmail.com>
2023-12-07 04:16:58 +0000

    Refactor search history functionality and add input validation

--- wordSeeker/src/App.tsx
+++ wordSeeker/src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
-import { Alert, Button, ButtonGroup, Stack, TextField, Tooltip, Typography } from '@mui/material'
+import { Alert, Button, Stack, TextField, Tooltip, Typography } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import SearchIcon from '@mui/icons-material/Search'
 import Footer from '@Components/Footer'
 import Header from '@Components/Header'
 import ContainerGrid from '@Pages/ContainerGrid'
 import ContextProvider from '@Providers/ContextProvider'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 export default function App(): JSX.Element {
   const [word, setWord] = useState<string | null>('')
   const searchHistory: string = localStorage.getItem('searchHistory') ?? ''
-  const [wordList, setWordList] = useState<string[]>(searchHistory.split('|||'))
+  const [history, setHistory] = useState<string[]>(searchHistory.split('|||'))
+  const wordRegex = /^[a-z]+$/
 
-  const handleSearch = (): void => {
-    if (word !== null && word.trim() !== '') {
-      setWordList((prevWordList) => {
+  function isValidWord(): boolean {
+    return wordRegex.test(word.trim().toLowerCase())
+  }
+
+  const handleHistory = (): void => {
+    if (word !== null && word.trim() !== '' && !history.includes(word)) {
+      setHistory((prevWordList) => {
         const updatedWordList = [word, ...prevWordList.slice(0, 4)]
         localStorage.setItem('searchHistory', updatedWordList.join('|||'))
         return updatedWordList
@@ -22,44 +28,85 @@ export default function App(): JSX.Element {
     }
   }
 
-  const formatWord = (longWord: string): string => longWord.substring(0, 12) + '...'
+  const handleSearch = (): void => {
+    if (word != null && !history.includes(word)) {
+      if (isValidWord()) {
+        // The word expected to be valid
+        console.log(`VALID WORD:${word}`)
+        handleHistory()
+      } else {
+        // The word expected to be invalid
+        console.log(`INVALID WORD: ${word}`)
+      }
+    }
+  }
+
+  const handleSpecialKeyPress = (e: React.KeyboardEvent<HTMLDivElement>): void => {
+    if (e.key === 'Enter') handleSearch()
+  }
+
+  const handleClearHistory = (): void => {
+    localStorage.removeItem('searchHistory')
+    setHistory([])
+  }
+
+  const formatWord = (longWord: string): string => {
+    if (longWord.length >= 15) return longWord.substring(0, 12) + '...'
+    else return longWord
+  }
 
   return (
     <ContextProvider>
       <Header />
       <ContainerGrid>
-        <Stack direction="row" alignItems="center" sx={{ pb: 2 }}>
+        <Stack direction="row" alignItems="center" sx={{ py: 3 }}>
           <TextField
             label="Seek a word..."
             variant={word !== undefined ? 'filled' : 'standard'}
             fullWidth
             onChange={(e) => setWord(e.target.value)}
+            onKeyDown={handleSpecialKeyPress}
             value={word ?? ''}
           />
           <IconButton color="secondary" sx={{ pl: 2 }} onClick={handleSearch}>
             <SearchIcon />
           </IconButton>
         </Stack>
-        {wordList !== null && wordList.length > 0 ? (
+        {history !== null && history.length > 0 ? (
           <>
-            <Typography variant="caption">Last 5 seeks</Typography>
-            <ButtonGroup variant="contained" color="secondary" fullWidth>
-              {wordList.map((w) => {
+            <Stack direction="row" spacing={2}>
+              <Typography variant="body1">
+                Last 5 seeks
+                <IconButton color="error" sx={{ pb: 1.5 }} onClick={handleClearHistory}>
+                  <DeleteIcon />
+                </IconButton>
+              </Typography>
+            </Stack>
+
+            <Stack direction="row">
+              {history.map((w) => {
                 if (w === '') return <></>
                 return (
                   <Tooltip title={w}>
-                    <Button key={w} onClick={() => setWord(w)}>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      sx={{ textOverflow: 'ellipsis', overflow: 'hidden' }}
+                      key={w}
+                      onClick={() => setWord(w)}
+                      fullWidth
+                    >
                       {formatWord(w)}
                     </Button>
                   </Tooltip>
                 )
               })}
-            </ButtonGroup>
+            </Stack>
           </>
         ) : (
           <></>
         )}
-        {word !== null && word.trim() !== '' ? (
+        {isValidWord() && history.includes(word ?? '') ? (
           <Alert severity="success">
             This is a word— check it out! <Typography>{word}</Typography>
           </Alert>
